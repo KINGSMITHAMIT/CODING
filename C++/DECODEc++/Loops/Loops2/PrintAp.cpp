5c7f60f079a19f1c1f0e043581abc855403875fb
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2023-09-11 06:55:25 +0530

    just trying to make the output look better

---
+++ C++/DECODEc++/Loops/Loops2/PrintAp.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+cout << "RADHE_RADHE" << endl;
+int n;
+cout<<"Enter the n num till which you want print your AP"<<endl;
+cin >> n;
+//METHOD--1
+// a=1; d = 2; n = __; 
+// a(n)= a -(n-1)d; --> a(n) = 2n-1
+for(int i=1; i<=(2*n-1); i+=2){
+    cout<< i <<" ";
+}
+cout<< endl;
+//METHOD--2 [better]
+int x=1;
+for (int i=1; i<=n; i++){
+    cout<< x <<" ";
+    x += 2;
+}
+return 0;
+}
