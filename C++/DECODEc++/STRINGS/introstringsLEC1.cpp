0a18e07d57f778a7ca12cad505d24438050b8d81
KINGSMITHAMIT <142286016+KINGSMITHAMIT@users.noreply.github.com>
2023-12-25 12:18:53 +0530

    First code to string.

---
+++ C++/DECODEc++/STRINGS/introstringsLEC1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+    void print(char arr[], int n){
+for (int i = 0; i < n;i++){
+    cout<<arr[i];
+}
+    }
+int main(){
+    char arr[] = {'R','a','d','h','e',' ','R','a','d','h','e'};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+    print(arr, n);
+return 0;
+}
